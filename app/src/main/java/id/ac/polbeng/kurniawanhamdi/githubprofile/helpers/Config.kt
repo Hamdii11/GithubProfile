ac066b1b845f4f6cfca6a7990d3b701f61c0ec09
Kurniawan Hamdi <kurniawanhamdi99@gmail.com>
2022-06-25 11:16:36 +0700

    finish

---
+++ app/src/main/java/id/ac/polbeng/kurniawanhamdi/githubprofile/helpers/Config.kt
@@ -0,0 +1,13 @@
+package id.ac.polbeng.kurniawanhamdi.githubprofile.helpers
+
+import id.ac.polbeng.kurniawanhamdi.githubprofile.BuildConfig
+
+class Config {
+    companion object{
+        const val SPLASH_SCREEN_DELAY:Long=3000
+        const val BASE_URL  ="https://api.github.com"
+        const val DEFAULT_USER_LOGIN = "Hamdi11"
+
+
+    }
+}
